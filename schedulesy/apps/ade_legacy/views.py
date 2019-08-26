86194aa9f8f536f98e47649edfd836d68d5507d5
Virgile Gerecke <virgile.gerecke@unistra.fr>
2019-08-26 16:53:42 +0200

    🔒 Now creates user for valid JWT

--- schedulesy/apps/ade_legacy/views.py
+++ schedulesy/apps/ade_legacy/views.py
@@ -1,4 +1,8 @@
+from uuid import uuid4
+
+from django.contrib.auth.models import User
 from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework_simplejwt.tokens import AccessToken
 
 from . import models
 from . import serializers
@@ -10,7 +14,20 @@ class CustomizationDetail(RetrieveUpdateDestroyAPIView):
     lookup_field = 'username'
 
     def perform_authentication(self, request):
-        m = request.META
+        meta = request.META
+        if 'HTTP_AUTHORIZATION' in meta and meta['HTTP_AUTHORIZATION'].startswith('Bearer '):
+            t = meta['HTTP_AUTHORIZATION'].split()[1]
+            try:
+                # Will create user if token is valid
+                token = AccessToken(t)
+                t_user = User.objects.get_or_create(username=token.get('user_id'),
+                                           defaults={'is_active': True})
+                if t_user[1]:
+                    t_user[0].set_password(str(uuid4()))
+                    t_user[0].save()
+            except Exception as e:
+                # Other controls will be made by call to super method
+                pass
         super().perform_authentication(request)
 
 
