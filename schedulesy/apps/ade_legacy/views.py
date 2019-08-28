ab94968d2d8b8a3c4e795da3cb013149a2f4e240
Thierry <thierry.schlecht@unistra.fr>
2019-08-28 15:26:14 +0200

    Custom JWT authentication

--- schedulesy/apps/ade_legacy/views.py
+++ schedulesy/apps/ade_legacy/views.py
@@ -1,12 +1,9 @@
-from uuid import uuid4
-
-from django.contrib.auth.models import User
 from django.core.exceptions import ObjectDoesNotExist
 from rest_framework import permissions
-from rest_framework.generics import RetrieveUpdateDestroyAPIView, ListCreateAPIView
+from rest_framework.generics import (
+    ListCreateAPIView, RetrieveUpdateDestroyAPIView)
 from rest_framework.response import Response
 from rest_framework.status import HTTP_404_NOT_FOUND
-from rest_framework_simplejwt.tokens import AccessToken
 
 from schedulesy.apps.ade_legacy.serializers import CustomizationSerializer
 from . import models
@@ -16,23 +13,6 @@ class CustomizationDetail(RetrieveUpdateDestroyAPIView):
     queryset = models.Customization.objects
     serializer_class = CustomizationSerializer
     lookup_field = 'username'
-
-    def perform_authentication(self, request):
-        meta = request.META
-        if 'HTTP_AUTHORIZATION' in meta and meta['HTTP_AUTHORIZATION'].startswith('Bearer '):
-            t = meta['HTTP_AUTHORIZATION'].split()[1]
-            try:
-                # Will create user if token is valid
-                token = AccessToken(t)
-                t_user = User.objects.get_or_create(username=token.get('user_id'),
-                                                    defaults={'is_active': True})
-                if t_user[1]:
-                    t_user[0].set_password(str(uuid4()))
-                    t_user[0].save()
-            except Exception as e:
-                # Other controls will be made by call to super method
-                pass
-        super().perform_authentication(request)
 
     def get(self, request, *args, **kwargs):
         try:
@@ -56,4 +36,3 @@ class CustomizationList(ListCreateAPIView):
             queryset = self.queryset.filter(username=user.username)
         serializer = CustomizationSerializer(queryset, many=True)
         return Response(serializer.data)
-
