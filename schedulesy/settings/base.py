51760b6502b61d13bbcb99e11ede8754a445bb3d
Yoran Hillion <yhillion@unistra.fr>
2019-09-05 14:16:08 +0200

    🔧 Corsheaders middleware added

--- schedulesy/settings/base.py
+++ schedulesy/settings/base.py
@@ -199,6 +199,7 @@ TEMPLATES = [
 ############################
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
