bb576458b7326f7594de33c4c799e92da19a96c5
Virgile Gerecke <virgile.gerecke@unistra.fr>
2019-09-12 10:19:23 +0200

    🐛 Fixed duplication on customization
    Now sends a 409 if object already exists

--- schedulesy/apps/ade_legacy/views.py
+++ schedulesy/apps/ade_legacy/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import (
     ListCreateAPIView, RetrieveUpdateDestroyAPIView)
 from rest_framework.response import Response
 from rest_framework.settings import api_settings
+from rest_framework.status import HTTP_409_CONFLICT
 
 from schedulesy.apps.ade_legacy.serializers import CustomizationSerializer
 from . import models
@@ -38,3 +39,11 @@ class CustomizationList(ListCreateAPIView):
         queryset = self.get_queryset().filter(**filters)
         serializer = CustomizationSerializer(queryset, many=True)
         return Response(serializer.data)
+
+    def post(self, request, *args, **kwargs):
+        filters = {'username': self.request.user}
+        queryset = self.get_queryset().filter(**filters)
+        if queryset.exists():
+            return Response({'detail':'Object already exists'},
+                            status=HTTP_409_CONFLICT)
+        return super().post(request, *args, **kwargs)
