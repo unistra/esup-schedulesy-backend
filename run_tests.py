18ca45ca31aba455284f2f0ff60caa0651fe889d
Thierry <thierry.schlecht@unistra.fr>
2019-09-26 10:29:24 +0200

    Enable tests on unmanaged models, part II

--- run_tests.py
+++ run_tests.py
@@ -5,21 +5,29 @@ import os
 import sys
 
 import django
-
-os.environ['DJANGO_SETTINGS_MODULE'] = 'schedulesy.settings.unittest'
-django.setup()
-
-from django.test.runner import DiscoverRunner
+from django.apps import apps
+from django.conf import settings
+from django.test.utils import get_runner
 
 
-"""
-Run tests script
-"""
+def manage_model(model):
+    model._meta.managed = True
 
-test_runner = DiscoverRunner(pattern='test_*.py', verbosity=2,
+
+if __name__ == '__main__':
+    os.environ['DJANGO_SETTINGS_MODULE'] = 'schedulesy.settings.unittest'
+    django.setup()
+    test_apps = settings.LOCAL_APPS
+
+    # Set all tested models to "managed = True"
+    for app in test_apps:
+        config = apps.get_app_config(app.split('.')[-1])
+        list(map(manage_model, config.get_models()))
+
+    test_apps = test_apps if len(sys.argv) <= 1 else sys.argv[1:]
+
+    TestRunner = get_runner(settings)
+    test_runner = TestRunner(pattern='test_*.py', verbosity=2,
                              interactive=True, failfast=False)
-
-test_apps = ['schedulesy']
-test_apps = test_apps if len(sys.argv) <= 1 else sys.argv[1:]
-failures = test_runner.run_tests(test_apps)
-sys.exit(failures)
+    failures = test_runner.run_tests(test_apps)
+    sys.exit(failures)
--- schedulesy/db_router.py
+++ schedulesy/db_router.py
@@ -1,10 +1,24 @@
 class DBRouter:
+
+    _alias = 'ade_legacy'
+
     def db_for_read(self, model, **hints):
-        if model._meta.app_label == 'ade_legacy':
+        if model._meta.app_label == self._alias:
             return 'ade'
         return 'default'
 
     def db_for_write(self, model, **hints):
-        if model._meta.app_label == 'ade_legacy':
+        if model._meta.app_label == self._alias:
             return 'ade'
         return 'default'
+
+    def allow_relation(self, obj1, obj2, **hints):
+        if obj1._meta.app_label == self._alias or \
+           obj2._meta.app_label == self._alias:
+            return True
+        return None
+
+    def allow_migrate(self, db, app_label, model_name=None, **hints):
+        if app_label == self._alias:
+            return db == 'ade'
+        return None
