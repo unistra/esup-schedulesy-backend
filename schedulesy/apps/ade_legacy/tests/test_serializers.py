b9b78372abd411daf5bb09332545ff75951f296b
Thierry <thierry.schlecht@unistra.fr>
2019-10-08 11:38:36 +0200

    Ref #15 : Testing get_ics_calendar

---
+++ schedulesy/apps/ade_legacy/tests/test_serializers.py
@@ -0,0 +1,35 @@
+from django.http import HttpRequest
+from django.test import TestCase
+
+from ..models import Customization
+from ..serializers import CustomizationSerializer
+
+
+class CustomizationSerializerTestCase(TestCase):
+
+    def setUp(self):
+        request = HttpRequest()
+        request.META = {
+            'SERVER_NAME': 'testserver.com',
+            'SERVER_PORT': 80
+        }
+        self.context = {'request': request}
+
+    def tearDown(self):
+        Customization.objects.all().delete()
+
+    def test_get_ics_calendar(self):
+        obj = Customization.objects.create(
+            id=1, directory_id='1', username='owner')
+        serializer = CustomizationSerializer(data=obj, context=self.context)
+        self.assertEqual(serializer.get_ics_calendar(obj),
+                         'http://testserver.com/unittest_media/owner.ics')
+
+    def test_get_ics_calendar_without_local_customization(self):
+        # Use the bulk create to prevent the call of the save method
+        Customization.objects.bulk_create([
+            Customization(id=1, directory_id='1', username='owner')
+        ])
+        obj = Customization.objects.get(id=1)
+        serializer = CustomizationSerializer(data=obj, context=self.context)
+        self.assertEqual(serializer.get_ics_calendar(obj), '')
