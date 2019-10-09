c22dda57893efa2e771213f255bd55f403781ee6
Thierry <thierry.schlecht@unistra.fr>
2019-10-09 10:06:18 +0200

    Testing pydiploy stuff

--- setup.py
+++ setup.py
@@ -30,7 +30,9 @@ def recursive_requirements(requirement_file, libs, links, path=''):
 
 libraries, dependency_links = [], []
 recursive_requirements('requirements.txt', libraries, dependency_links)
-version = __import__('schedulesy').get_version()
+# version = __import__('schedulesy').get_version()
+# DEBUG
+version = '1.1.0rc1'
 
 setup(
     name='schedulesy',
