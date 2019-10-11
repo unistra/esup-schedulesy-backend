70bb8ec0df6412d906173a12d87b392e2b604336
Thierry <thierry.schlecht@unistra.fr>
2019-10-11 15:13:42 +0200

    :white_check_mark: Add a test on the authentication

--- schedulesy/libs/tests/test_authentication.py
+++ schedulesy/libs/tests/test_authentication.py
@@ -39,7 +39,7 @@ class CustomJWTAuthenticationTestCase(TestCase):
             user.is_active = False
             user.save()
 
-            # # Should raise exception if user is inactive
+            # Should raise exception if user is inactive
             with self.assertRaises(AuthenticationFailed) as af:
                 self.backend.get_user(payload)
             self.assertEqual(af.exception.detail['code'], 'user_inactive')
@@ -55,6 +55,11 @@ class CustomJWTAuthenticationTestCase(TestCase):
         payload[api_settings.USER_ID_CLAIM] = 'jwt_new_user'
         jwt_settings['CREATE_USER'] = True
         with self.settings(SIMPLE_JWT=jwt_settings):
+            self.assertEqual(getattr(self.backend.get_user(payload),
+                                     api_settings.USER_ID_FIELD),
+                             'jwt_new_user')
+
+            # The user should not be recreated
             self.assertEqual(getattr(self.backend.get_user(payload),
                                      api_settings.USER_ID_FIELD),
                              'jwt_new_user')
