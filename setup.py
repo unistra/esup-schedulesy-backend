460ee50756fe7078d746fe374439a339bec45844
Virgile Gerecke <virgile.gerecke@unistra.fr>
2019-11-08 15:23:56 +0100

    🚀 Updated setup for deployment

--- setup.py
+++ setup.py
@@ -30,8 +30,12 @@ def recursive_requirements(requirement_file, libs, links, path=''):
 
 libraries, dependency_links = [], []
 recursive_requirements('requirements.txt', libraries, dependency_links)
-#version = __import__('schedulesy').get_version()
-version="1.1.0-rc"
+
+try:
+    version = __import__('schedulesy').get_version()
+except Exception as e:
+    version = open("build.txt", 'r').read()
+
 
 setup(
     name='schedulesy',
