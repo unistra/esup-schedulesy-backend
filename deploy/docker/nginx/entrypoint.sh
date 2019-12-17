0289dab93411eb59a38842c93fdd45a22be3faad
Thierry <thierry.schlecht@unistra.fr>
2019-12-17 10:39:00 +0100

    WIP - testing docker

---
+++ deploy/docker/nginx/entrypoint.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# TODO: reuse later !!!
+
+# if [ -n "$UWSGI_URI" ]; then
+# 	sed -e "s/{{UWSGI_URI}}/$UWSGI_URI/" config.conf > /etc/nginx/nginx.conf
+# else
+#   sed -e "s/{{UWSGI_URI}}/0.0.0.0:8080/" config.conf > /etc/nginx/nginx.conf
+# fi
+
+# exec "$@"
---
+++ deploy/docker/uwsgi/prestart.sh
@@ -0,0 +1,10 @@
+#! /usr/bin/env sh
+set -e
+echo "Running inside /app/prestart.sh"
+export DJANGO_SETTINGS_MODULE=djocker.settings.preprod
+# Don't stop building when compilemessages fail
+cd /app && python manage.py migrate && python manage.py compilemessages --settings=djocker.settings.preprod || true
+# TODO: collectstatic in shared volume for nginx !
+
+exec "$@"
+
