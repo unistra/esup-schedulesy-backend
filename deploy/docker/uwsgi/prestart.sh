fa16eb6d2482d4ae6cc7c8e1f18a469d3d511b68
Thierry <thierry.schlecht@unistra.fr>
2019-12-20 11:53:34 +0100

    Image build with test settings

--- deploy/docker/uwsgi/prestart.sh
+++ deploy/docker/uwsgi/prestart.sh
@@ -1,7 +1,7 @@
 #! /usr/bin/env sh
 set -e
 echo "Running inside /app/prestart.sh"
-export DJANGO_SETTINGS_MODULE=djocker.settings.preprod
+export DJANGO_SETTINGS_MODULE=djocker.settings.test
 # Don't stop building when compilemessages fail
 cd /app && python manage.py migrate && python manage.py compilemessages --settings=djocker.settings.test || true
 # TODO: collectstatic in shared volume for nginx !
