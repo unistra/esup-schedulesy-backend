450af103b5e37899e8680f4d820d97b2377b49bb
Thierry <thierry.schlecht@unistra.fr>
2020-01-07 15:04:41 +0100

    Testing collecstatic

--- deploy/docker/uwsgi/prestart.sh
+++ deploy/docker/uwsgi/prestart.sh
@@ -3,7 +3,10 @@ set -e
 echo "Running inside /app/prestart.sh"
 export DJANGO_SETTINGS_MODULE=schedulesy.settings.test
 # Don't stop building when compilemessages fail
-cd /app && python manage.py migrate && python manage.py compilemessages --settings=schedulesy.settings.test || true
+cd app
+python manage.py migrate
+python manage.py compilemessages --settings=schedulesy.settings.test || true
+python manage.py collectstatic
 # TODO: collectstatic in shared volume for nginx !
 
 exec "$@"
--- schedulesy/settings/base.py
+++ schedulesy/settings/base.py
@@ -132,7 +132,7 @@ MEDIA_URL = '/media/'
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = normpath(join(SITE_ROOT, 'assets'))
+STATIC_ROOT = '/static'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
