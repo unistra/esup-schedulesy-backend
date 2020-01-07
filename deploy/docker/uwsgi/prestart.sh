e45cbe9a4ed98ffeb7a7bf5d39788363088d1453
Thierry <thierry.schlecht@unistra.fr>
2020-01-07 15:40:23 +0100

    Typos

--- deploy/docker/uwsgi/prestart.sh
+++ deploy/docker/uwsgi/prestart.sh
@@ -3,7 +3,7 @@ set -e
 echo "Running inside /app/prestart.sh"
 export DJANGO_SETTINGS_MODULE=schedulesy.settings.test
 # Don't stop building when compilemessages fail
-cd app
+cd /app
 python manage.py migrate
 python manage.py compilemessages --settings=schedulesy.settings.test || true
 python manage.py collectstatic
