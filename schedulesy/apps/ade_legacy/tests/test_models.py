515cbba4362ded7bfaa7bc23601b5a0f28743b24
Virgile Gerecke <virgile.gerecke@unistra.fr>
2020-02-28 14:11:06 +0100

    ✅ Full coverage of legacy model

--- schedulesy/apps/ade_legacy/tests/test_models.py
+++ schedulesy/apps/ade_legacy/tests/test_models.py
@@ -12,6 +12,18 @@ class CustomizationModelTestCase(TestCase):
         super().tearDown()
         Customization.objects.all().delete()
 
+    def test_read_without_local_customization(self):
+        customization = Customization.objects.create(
+            id=1, resources='10', directory_id='42', username='user1')
+        customization.configuration='{"mode":"dark"}'
+        # The 'save' method automatically creates missing LocalCustomization
+        # To simulate an oprhan line in Customization, we have to delete the LocalCustomization
+        LocalCustomization.objects.get(customization_id=1).delete()
+        lc = customization.local_customization
+        self.assertIsInstance(lc, LocalCustomization)
+        self.assertEqual(lc.resources.count(), 1)
+        self.assertTrue(lc.resources.filter(ext_id='10').exists())
+
     def test_save_without_local_customization(self):
         Customization.objects.create(
             id=1, resources='10', directory_id='42', username='user1')
@@ -44,3 +56,12 @@ class CustomizationModelTestCase(TestCase):
             id=1, resources='11,12', directory_id='42', username='user1')
 
         self.assertEqual(local.resources.count(), 2)
+
+    def test_ics_calendar(self):
+        customization = Customization.objects.create(
+            id=1, resources='11,12', directory_id='42', username='user1')
+        self.assertEqual("409a1639a5f7496086822b5f15e0e5bca85c161f.ics", customization.ics_calendar)
+
+    def test_str(self):
+        local = LocalCustomization.objects.create(customization_id=1, username='user1')
+        self.assertEqual('user1', local.__str__())
