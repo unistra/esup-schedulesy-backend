4fe69c03b9ddbebc06a8ba57a5de123cfc727c53
Thierry Schlecht <thierry.schlecht@unistra.fr>
2025-12-01 08:51:14 +0100

    Ref #44 - Restore the old db_column values in the Customization model

--- schedulesy/apps/ade_legacy/models.py
+++ schedulesy/apps/ade_legacy/models.py
@@ -7,14 +7,14 @@ from schedulesy.apps.ade_api import models as api
 
 
 class Customization(models.Model):
-    id = models.AutoField(primary_key=True)
-    display_configuration = models.CharField(max_length=60, blank=True)
-    resources = models.CharField(max_length=300, blank=True)
-    directory_id = models.CharField(max_length=32, unique=True)
-    rh_id = models.CharField(max_length=15, blank=True)
-    creation_date = models.DateTimeField(auto_now_add=True, blank=True, null=True)
-    customization_date = models.DateTimeField(auto_now=True, blank=True, null=True)
-    username = models.CharField(max_length=32, blank=True)
+    id = models.AutoField(primary_key=True, db_column='id')
+    display_configuration = models.CharField(max_length=60, blank=True, db_column='configuration_affichage')
+    resources = models.CharField(max_length=300, blank=True, db_column='ressources')
+    directory_id = models.CharField(max_length=32, unique=True, db_column='uds_directory_id')
+    rh_id = models.CharField(max_length=15, blank=True, db_column='code_harp')
+    creation_date = models.DateTimeField(auto_now_add=True, blank=True, null=True, db_column='date_creation')
+    customization_date = models.DateTimeField(auto_now=True, blank=True, null=True, db_column='date_personnalisation')
+    username = models.CharField(max_length=32, blank=True, db_column='uid')
 
     @property
     def ics_calendar(self):
@@ -84,6 +84,7 @@ class Customization(models.Model):
         return lc
 
     class Meta:
+        db_table = 'w_edtperso'
         verbose_name = _('Customization')
         verbose_name_plural = _('Customizations')
 
